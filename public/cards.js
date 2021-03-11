be608262e438446a77bf376d1cb131050118bb0b
Max Baldwin <maxrbaldwin2328@gmail.com>
2021-03-11 17:03:09 -0500

    add call cards. use wix instead of cloudinary

--- public/cards.js
+++ public/cards.js
@@ -1,125 +1,156 @@
-const defaultBack = 'https://res.cloudinary.com/maxrbaldwin-com/image/upload/v1613072069/Wix-LightScatterPress/back-of-card-draft.jpg';
+const defaultAudio = 'https://static.wixstatic.com/mp3/bb0dab_3a8357b076cf43db8c3b6a1f7a9d21d1.wav';
+
+const purple = 'https://static.wixstatic.com/media/bb0dab_1db200237d1e4ba49b36546343a119a2~mv2.jpg';
+const darkCherry = 'https://static.wixstatic.com/media/bb0dab_8189a76c5c454de89666d46ec451d885~mv2.jpg';
+const fiveZeroSix = 'https://static.wixstatic.com/media/bb0dab_d92a1867818140708252c09a6199c0cf~mv2.jpg';
+const darkOrange = 'https://static.wixstatic.com/media/bb0dab_7da48dabb2d842da917d935eaffac715~mv2.jpg';
+const paleYellow = 'https://static.wixstatic.com/media/bb0dab_258a81e580f34b5bafa95c706a63b14b~mv2.jpg';
+const gold = 'https://static.wixstatic.com/media/bb0dab_eaea0a9bc2d74f81b567018f579c8138~mv2.jpg';
+const green = 'https://static.wixstatic.com/media/bb0dab_b08f5c18cfbc431e9ce77a8154450cda~mv2.jpg';
+const threeFourteen = 'https://static.wixstatic.com/media/bb0dab_1913c50d6df545609452496421345b01~mv2.jpg';
+const leviBlue = 'https://static.wixstatic.com/media/bb0dab_5a839d11cc624202b56afd4ee91b656e~mv2.jpg';
+const pink = 'https://static.wixstatic.com/media/bb0dab_319d4fe4eeb5465baf2ff03e74dd7449~mv2.jpg';
+const periwinkle = 'https://static.wixstatic.com/media/bb0dab_1f07e580cf2849ec9425e09c8871ad69~mv2.jpg';
+const grayBrown = 'https://static.wixstatic.com/media/bb0dab_deffce6717cc4682846a56ce26d78a36~mv2.jpg';
+const gray = 'https://static.wixstatic.com/media/bb0dab_deffce6717cc4682846a56ce26d78a36~mv2.jpg';
+const sage = 'https://static.wixstatic.com/media/bb0dab_8077eac9ce2640df91f9d04f4cd5e2e5~mv2.jpg';
+const red = 'https://static.wixstatic.com/media/bb0dab_4e8b6d19c342450788c37282cc8be61b~mv2.jpg';
 
 export const data = [
   {
     id: '10001',
     title: 'card 1',
     desc: "Here is card one",
-    backColor: defaultBack,
-    front: 'https://res.cloudinary.com/maxrbaldwin-com/image/upload/v1614026074/Wix-LightScatterPress/Water-and-Stone-Final.png',
-    audio: 'https://res.cloudinary.com/maxrbaldwin-com/video/upload/v1612988055/Wix-LightScatterPress/Gaia_2.wav',
+    backColor: leviBlue,
+    front: 'https://static.wixstatic.com/media/bb0dab_59c738026b594f38bffecde74fbe0c2b~mv2.png',
+    audio: defaultAudio,
+    pageNumber: 100,
   },
   {
     id: '10002',
     title: 'card 2',
     desc: "Here is card two",
-    backColor: defaultBack,
-    front: 'https://res.cloudinary.com/maxrbaldwin-com/image/upload/v1614026074/Wix-LightScatterPress/August-Final.png',
-    audio: 'https://res.cloudinary.com/maxrbaldwin-com/video/upload/v1612988055/Wix-LightScatterPress/Gaia_2.wav',
+    backColor: periwinkle,
+    front: 'https://static.wixstatic.com/media/bb0dab_5e7bf9b2e04a440d88ffb636ba971350~mv2.png',
+    audio: defaultAudio,
+    pageNumber: 100,
   },
   {
     id: '10003',
     title: 'card 3',
     desc: "Here is card three",
-    backColor: defaultBack,
-    front: 'https://res.cloudinary.com/maxrbaldwin-com/image/upload/v1614026074/Wix-LightScatterPress/Three-Things-Final.png',
-    audio: 'https://res.cloudinary.com/maxrbaldwin-com/video/upload/v1612988055/Wix-LightScatterPress/Gaia_2.wav',
+    backColor: gold,
+    front: 'https://static.wixstatic.com/media/bb0dab_69f853a6dbd747e08d046d201c719661~mv2.png',
+    audio: defaultAudio,
+    pageNumber: 100,
   },
   {
     id: '10004',
     title: 'card 4',
     desc: "Here is card four",
-    backColor: defaultBack,
-    front: 'https://res.cloudinary.com/maxrbaldwin-com/image/upload/v1614026070/Wix-LightScatterPress/afterlife.png',
-    audio: 'https://res.cloudinary.com/maxrbaldwin-com/video/upload/v1612988055/Wix-LightScatterPress/Gaia_2.wav',
+    backColor: gray,
+    front: 'https://static.wixstatic.com/media/bb0dab_0ba90dd715424bd7b9f1ae63887e9ec6~mv2.png',
+    audio: defaultAudio,
+    pageNumber: 100,
   },
   {
     id: '10005',
     title: 'card 5',
     desc: "Here is card five",
-    backColor: defaultBack,
-    front: 'https://res.cloudinary.com/maxrbaldwin-com/image/upload/v1612988042/Wix-LightScatterPress/gaia.jpg',
-    audio: 'https://res.cloudinary.com/maxrbaldwin-com/video/upload/v1612988055/Wix-LightScatterPress/Gaia_2.wav',
+    backColor: red,
+    front: 'https://static.wixstatic.com/media/bb0dab_c85d99cdef384dda8c838fdaf5e22cf2~mv2.png',
+    audio: defaultAudio,
+    pageNumber: 100,
   },
   {
     id: '10006',
     title: 'card 6',
     desc: "Here is card six",
-    backColor: defaultBack,
-    front: 'https://res.cloudinary.com/maxrbaldwin-com/image/upload/v1614026070/Wix-LightScatterPress/Wishbone.png',
-    audio: 'https://res.cloudinary.com/maxrbaldwin-com/video/upload/v1612988055/Wix-LightScatterPress/Gaia_2.wav',
+    backColor: paleYellow,
+    front: 'https://static.wixstatic.com/media/bb0dab_af2d252da6b646a1bf91fa3f73e283e6~mv2.png',
+    audio: defaultAudio,
+    pageNumber: 100,
   },
   {
     id: '10007',
     title: 'card 7',
     desc: "Here is card seven",
-    backColor: defaultBack,
-    front: 'https://res.cloudinary.com/maxrbaldwin-com/image/upload/v1614026069/Wix-LightScatterPress/sevenoclock.png',
-    audio: 'https://res.cloudinary.com/maxrbaldwin-com/video/upload/v1612988055/Wix-LightScatterPress/Gaia_2.wav',
+    backColor: green,
+    front: 'https://static.wixstatic.com/media/bb0dab_396ea82db4bb459bb18a4b18f542fea5~mv2.png',
+    audio: defaultAudio,
+    pageNumber: 100,
   },
   {
     id: '10008',
     title: 'card 8',
     desc: "Here is card eight",
-    backColor: defaultBack,
-    front: 'https://res.cloudinary.com/maxrbaldwin-com/image/upload/v1614026069/Wix-LightScatterPress/In-The-Big-House-Giraffe-Final.png',
-    audio: 'https://res.cloudinary.com/maxrbaldwin-com/video/upload/v1612988055/Wix-LightScatterPress/Gaia_2.wav',
+    backColor: fiveZeroSix,
+    front: 'https://static.wixstatic.com/media/bb0dab_4523e236f68d414faa36ffe33412903f~mv2.png',
+    audio: defaultAudio,
+    pageNumber: 100,
   },
   {
     id: '10009',
     title: 'card 9',
     desc: "Here is card nine",
-    backColor: defaultBack,
-    front: 'https://res.cloudinary.com/maxrbaldwin-com/image/upload/v1614026069/Wix-LightScatterPress/verysmall.png',
-    audio: 'https://res.cloudinary.com/maxrbaldwin-com/video/upload/v1612988055/Wix-LightScatterPress/Gaia_2.wav',
+    backColor: purple,
+    front: 'https://static.wixstatic.com/media/bb0dab_4a43624b165c4a368a9ad8caa8509960~mv2.png',
+    audio: defaultAudio,
+    pageNumber: 100,
   },
   {
     id: '10010',
     title: 'card 10',
     desc: "Here is card ten",
-    backColor: defaultBack,
-    front: 'https://res.cloudinary.com/maxrbaldwin-com/image/upload/v1614026069/Wix-LightScatterPress/twelve.png',
-    audio: 'https://res.cloudinary.com/maxrbaldwin-com/video/upload/v1612988055/Wix-LightScatterPress/Gaia_2.wav',
+    backColor: darkCherry,
+    front: 'https://static.wixstatic.com/media/bb0dab_d0fedb61665643fa945804c90111dd19~mv2.png',
+    audio: defaultAudio,
+    pageNumber: 100,
   },
   {
     id: '10011',
     title: 'card 11',
     desc: "Here is card eleven",
-    backColor: defaultBack,
-    front: 'https://res.cloudinary.com/maxrbaldwin-com/image/upload/v1614026068/Wix-LightScatterPress/nature-of-objects.png',
-    audio: 'https://res.cloudinary.com/maxrbaldwin-com/video/upload/v1612988055/Wix-LightScatterPress/Gaia_2.wav',
+    backColor: darkOrange,
+    front: 'https://static.wixstatic.com/media/bb0dab_c3c0c3ca21df4f2caafa90e1fb7192bb~mv2.png',
+    audio: defaultAudio,
+    pageNumber: 100,
   },
   {
     id: '10012',
     title: 'card 12',
     desc: "Here is card twelve",
-    backColor: defaultBack,
-    front: 'https://res.cloudinary.com/maxrbaldwin-com/image/upload/v1614026068/Wix-LightScatterPress/ballistics.png',
-    audio: 'https://res.cloudinary.com/maxrbaldwin-com/video/upload/v1612988055/Wix-LightScatterPress/Gaia_2.wav',
+    backColor: pink,
+    front: 'https://static.wixstatic.com/media/bb0dab_c33b820432aa497fb5853c6172561b14~mv2.png',
+    audio: defaultAudio,
+    pageNumber: 100,
   },
   {
     id: '10013',
     title: 'card 13',
     desc: "Here is card thirteen",
-    backColor: defaultBack,
-    front: 'https://res.cloudinary.com/maxrbaldwin-com/image/upload/v1614026068/Wix-LightScatterPress/Bacopa-Final-V2.png',
-    audio: 'https://res.cloudinary.com/maxrbaldwin-com/video/upload/v1612988055/Wix-LightScatterPress/Gaia_2.wav',
+    backColor: threeFourteen,
+    front: 'https://static.wixstatic.com/media/bb0dab_da7721d62c5b4a82b7aa3455631f9e2b~mv2.png',
+    audio: defaultAudio,
+    pageNumber: 100,
   },
   {
     id: '10014',
     title: 'card 14',
     desc: "Here is card fourteen",
-    backColor: defaultBack,
-    front: 'https://res.cloudinary.com/maxrbaldwin-com/image/upload/v1614026067/Wix-LightScatterPress/This-Morning-Final.png',
-    audio: 'https://res.cloudinary.com/maxrbaldwin-com/video/upload/v1612988055/Wix-LightScatterPress/Gaia_2.wav',
+    backColor: sage,
+    front: 'https://static.wixstatic.com/media/bb0dab_640c24da34364ca8bdbc56ea8cdb15c6~mv2.png',
+    audio: defaultAudio,
+    pageNumber: 100,
   },
   {
     id: '10015',
     title: 'card 15',
     desc: "Here is card fifteen",
-    backColor: defaultBack,
-    front: 'https://res.cloudinary.com/maxrbaldwin-com/image/upload/v1614026067/Wix-LightScatterPress/practical.png',
-    audio: 'https://res.cloudinary.com/maxrbaldwin-com/video/upload/v1612988055/Wix-LightScatterPress/Gaia_2.wav',
+    backColor: grayBrown,
+    front: 'https://static.wixstatic.com/media/bb0dab_3dd171d451634572b89449377e660e29~mv2.png',
+    audio: defaultAudio,
+    pageNumber: 100,
   },
 ];
 
@@ -136,6 +167,7 @@ class cards {
   constructor(firstCard) {
     this.data = makeDeck(firstCard);
     this.currentCard = firstCard.toString();
+    this.firstCard = firstCard;
   }
   selectCard(card) {
     const clone = this.data;
