1cd40aa456bbc1efab7fed1dca935961ad3e647c
Max Baldwin <maxrbaldwin2328@gmail.com>
2021-03-11 17:03:34 -0500

    add bookmark and shuffle functionality

--- pages/page.js
+++ pages/page.js
@@ -30,12 +30,6 @@ const pageCommands = {
             .catch(err => {
                 console.log('Catch in open directory modal: ', err);
             });
-    },
-    nextCard: function (deck) {
-        return deck.nextCard();
-    },
-    previousCard: function(deck) {
-        return deck.previousCard();
     }
 };
 
@@ -43,6 +37,9 @@ $w.onReady(function () {
     const queryParamCard = (wixLocation.query && wixLocation.query.card) || defaultCard;
     const deck = new cards(queryParamCard);
 	const viewport = wixWindow.formFactor.toLocaleLowerCase();
+    const firstCardData = JSON.stringify(deck.getCardInfo(deck.firstCard.toString()));
+    let bookmark = 1;
+    let shuffle = 1;
 
     // open card modal from carousel
     $w(appCarouselSelector).on('open-card-modal', function(e) {
@@ -57,7 +54,43 @@ $w.onReady(function () {
         pageCommands.openDirectoryModal(deck);
     });
 
+    $w(appNavigationSelector).on('bookmark', function(e) {
+        const getRefresh = () => {
+            let temp;
+            if (bookmark === 1) {
+                temp = 2;
+                bookmark = 2;
+            } else if (bookmark === 2) {
+                temp = 1;
+                bookmark = 1;
+            }
+            return temp;
+        }
+        // hack to force the deck to change the first card when the "card" attr doesn't change
+        $w(appCarouselSelector).setAttribute('bookmark', getRefresh());
+        $w(appCarouselSelector).setAttribute('card', firstCardData);
+    });
+
+    $w(appNavigationSelector).on('shuffle', function(e) {
+        const getShuffle = () => {
+            let temp;
+            if (shuffle === 1) {
+                temp = 2;
+                shuffle = 2;
+            } else if (shuffle === 2) {
+                temp = 1;
+                shuffle = 1;
+            }
+            return temp;
+        }
+
+        $w(appCarouselSelector).setAttribute('shuffle', getShuffle());
+    })
+
 	$w(appCarouselSelector).setAttribute('deck', JSON.stringify(deck));
-    // $w(appCarouselSelector).setAttribute('card', deck.currentCard);
+    $w(appCarouselSelector).setAttribute('bookmark', bookmark);
+    $w(appCarouselSelector).setAttribute('shuffle', shuffle);
     $w(appCarouselSelector).setAttribute('viewport', viewport);
+    $w(appNavigationSelector).setAttribute('viewport', viewport);
+    $w(appNavigationSelector).setAttribute('card', firstCardData);
 });
