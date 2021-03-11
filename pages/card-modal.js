d2d19d2b5d0c02b838fff69854cec9eec765e1c6
Max Baldwin <maxrbaldwin2328@gmail.com>
2021-03-11 17:04:18 -0500

    add viewport. close modal

--- pages/card-modal.js
+++ pages/card-modal.js
@@ -2,5 +2,11 @@ import wixWindow from 'wix-window';
 
 $w.onReady(function () {
 	const context = wixWindow.lightbox.getContext();
+	const vp = wixWindow.formFactor
 	$w('#CardModal').setAttribute('card', JSON.stringify(context.card));
+	$w('#CardModal').setAttribute('viewport', vp);
+
+	$w('#CardModal').on('close-card-modal', () => {
+		wixWindow.lightbox.close();
+	})
 });
